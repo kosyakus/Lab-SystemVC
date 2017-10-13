756b1b9546f759ee18c16bebabf2b2e991a5764e
Unknown <kosyak_@mail.ru>
2017-10-13 16:30:13 +0300

    Created Controllers for TV and V

---
+++ Lab-HomeFurniture/FurnitureDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FurnitureDetailViewController.swift
+//  Lab-HomeFurniture
+//
+//  Created by Admin on 13.10.17.
+//  Copyright © 2017 NS. All rights reserved.
+//
+
+import UIKit
+
+class FurnitureDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
