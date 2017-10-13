cf7492d714f034dc484c0a6b0a35728d82fc1cc4
Unknown <kosyak_@mail.ru>
2017-10-14 01:40:05 +0300

    Added image picker and nav contr delegates

--- Lab-HomeFurniture/FurnitureDetailViewController.swift
+++ Lab-HomeFurniture/FurnitureDetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FurnitureDetailViewController: UIViewController {
+class FurnitureDetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var choosePhotoButton: UIButton!
     
